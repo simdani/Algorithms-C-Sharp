29bf03d07962dac6385a939f89eb1426d31441b8
Simonas Daniliauskas <dranxman@gmail.com>
2018-08-23 21:11:12 +0300

    Linked list is palindrome

--- UnitTests/LinkedListsTests.cs
+++ UnitTests/LinkedListsTests.cs
@@ -124,5 +124,25 @@ namespace UnitTests
                 expectedList = expectedList.next;
             }
         }
+
+        [Fact]
+        public void TestPalindrome()
+        {
+            int[] l1 = new int[] { 0, 1, 2, 1, 0 };
+            int[] l2 = new int[] { 0, 1, 2, 0};
+
+            // solution #1 traverse and clone
+            LinkedListNode l1List = LinkedListNode.BuildList(l1);
+            Assert.True(Palindrome.IsPalindrome(l1List));
+
+            LinkedListNode l2List = LinkedListNode.BuildList(l2);
+            Assert.False(Palindrome.IsPalindrome(l2List));
+
+            // solution #2 iterative
+            Assert.True(Palindrome.IsPalindromeIterative(l1List));
+
+            // solution #3 recursive
+            Assert.True(Palindrome.IsPalindromeRecursive(l1List));
+        }
     }
 }
