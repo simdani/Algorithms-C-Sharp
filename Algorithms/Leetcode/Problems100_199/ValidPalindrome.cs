2de616cf127fee9ec5cee5a87d56d40b54a53e4e
Simonas Daniliauskas <dranxman@gmail.com>
2018-09-03 14:18:33 +0300

    Valid palindrome leetcode problem solution

---
+++ Algorithms/Leetcode/Problems100_199/ValidPalindrome.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Leetcode.Problems100_199
+{
+    public class ValidPalindrome
+    {
+        // my solution
+        public bool IsPalindrome(string s)
+        {
+            if (s.Length == 0) return true;
+
+            char[] arr = s.ToLower().Where(c => (char.IsLetterOrDigit(c))).ToArray();
+
+            string modified = new string(arr);
+
+            int back = modified.Length - 1;
+            for (int i = 0; i < modified.Length / 2; i++)
+            {
+                if (modified[i] != modified[back])
+                {
+                    return false;
+                }
+
+                back--;
+            }
+            return true;
+        }
+
+        // improved performance solution
+        public bool IsPalindromeInPlace(string s)
+        {
+            if (s.Length == 0) return true;
+            int i = 0;
+            int j = s.Length - 1;
+            while (i < j)
+            {
+                while (i < j && !char.IsLetterOrDigit(s[i]))
+                    i++;
+                while (i < j && !char.IsLetterOrDigit(s[j]))
+                    j--;
+                if (char.ToLower(s[i++]) != char.ToLower(s[j--]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnitTests/LeetcodeTests/Problems100_199/ValidPalindromeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Leetcode.Problems100_199;
+using Xunit;
+
+namespace UnitTests.LeetcodeTests.Problems100_199
+{
+    public class ValidPalindromeTest
+    {
+        [Fact]
+        public void TestValidPalindrome()
+        {
+            ValidPalindrome palindrome = new ValidPalindrome();
+            Assert.True(palindrome.IsPalindromeInPlace("A man, a plan, a canal: Panama"));
+            Assert.True(palindrome.IsPalindrome("neen"));
+            Assert.True(palindrome.IsPalindrome("kemek"));
+            Assert.False(palindrome.IsPalindrome("race a car"));
+        }
+    }
+}
