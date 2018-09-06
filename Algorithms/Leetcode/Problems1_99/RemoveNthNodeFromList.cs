7307a4fb6988f32672c04f7a63bc0661b83daee9
Simonas Daniliauskas <dranxman@gmail.com>
2018-09-06 22:28:47 +0300

    Remove nth from end problem

---
+++ Algorithms/Leetcode/Problems1_99/RemoveNthNodeFromList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Leetcode.Problems200_299;
+
+namespace Algorithms.Leetcode.Problems1_99
+{
+    public class RemoveNthNodeFromList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while (fast != null)
+            {
+                fast = fast.next;
+                if (n-- < 0)
+                {
+                    slow = slow.next;
+                }
+            }
+
+            if (n == 0)
+            {
+                head = head.next;
+            } else if (n < 0)
+            {
+                slow.next = slow.next.next;
+            }
+            else
+            {
+                return null;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ UnitTests/LeetcodeTests/Problems1_99/RemoveNthFromEndTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Leetcode.Problems1_99;
+using Algorithms.Leetcode.Problems200_299;
+using Xunit;
+
+namespace UnitTests.LeetcodeTests.Problems1_99
+{
+    public class RemoveNthFromEndTest
+    {
+        [Fact]
+        public void TestRemoveNthFromEnd()
+        { 
+            RemoveNthNodeFromList remove = new RemoveNthNodeFromList();
+
+            ListNode firstNode = new ListNode(1);
+            ListNode secondNode = new ListNode(2);
+            ListNode thirdNode = new ListNode(3);
+            ListNode fourthNode = new ListNode(4);
+            ListNode fifthNode = new ListNode(5);
+
+            firstNode.next = secondNode;
+            firstNode.next.next = thirdNode;
+            firstNode.next.next.next = fourthNode;
+            firstNode.next.next.next.next = fifthNode;
+
+            remove.RemoveNthFromEnd(firstNode, 2);
+
+            Assert.Equal(1, firstNode.val);
+            Assert.Equal(2, firstNode.next.val);
+            Assert.Equal(3, firstNode.next.next.val);
+            Assert.Equal(5, firstNode.next.next.next.val);
+        }
+    }
+}
