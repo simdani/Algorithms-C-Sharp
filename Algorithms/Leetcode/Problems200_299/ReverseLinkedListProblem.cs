d736c730c9ffbd620451c791e543c0a763c015ab
Simonas Daniliauskas <dranxman@gmail.com>
2018-09-13 22:30:11 +0300

    Add reverseList Leetcode problem solution

---
+++ Algorithms/Leetcode/Problems200_299/ReverseLinkedListProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Leetcode.Problems200_299
+{
+    public class ReverseLinkedListProblem
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode current = head;
+            ListNode next = null;
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            head = prev;
+            return head;
+        }
+    }
+}
---
+++ UnitTests/LeetcodeTests/Problems200_299/ReverseLinkedListTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Leetcode.Problems200_299;
+using Xunit;
+
+namespace UnitTests.LeetcodeTests.Problems200_299
+{
+    public class ReverseLinkedListTest
+    {
+        [Fact]
+        public void TestReverseLinkedList()
+        {
+            ListNode first = new ListNode(1);
+            ListNode second = new ListNode(2);
+            ListNode third = new ListNode(3);
+
+            first.next = second;
+            first.next.next = third;
+
+            ReverseLinkedListProblem reverse = new ReverseLinkedListProblem();
+            reverse.ReverseList(first);
+        }
+    }
+}
