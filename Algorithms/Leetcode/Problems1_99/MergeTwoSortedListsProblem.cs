e53ee06f4b5bbb00578f14d1bce7b4cc8ac35746
Simonas Daniliauskas <dranxman@gmail.com>
2018-09-14 22:12:40 +0300

    Merge two sorted lists algorithm

---
+++ Algorithms/Leetcode/Problems1_99/MergeTwoSortedListsProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Leetcode.Problems200_299;
+
+namespace Algorithms.Leetcode.Problems1_99
+{
+    public class MergeTwoSortedListsProblem
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+
+            if (l1.val < l2.val)
+            {
+                l1.next = MergeTwoLists(l1.next, l2);
+                return l1;
+            }
+            else
+            {
+                l2.next = MergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+}
