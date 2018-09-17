a1543c0ff8adc7cdf5b229b709b960560404171f
Simonas Daniliauskas <dranxman@gmail.com>
2018-09-17 21:41:12 +0300

    Leetcode Palindrome linked list solution

---
+++ Algorithms/Leetcode/Problems200_299/PalindromeLinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Leetcode.Problems200_299
+{
+    public class PalindromeLinkedList
+    {
+        public Boolean IsPalindrome(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            if (fast != null)
+            {
+                slow = slow.next;
+            }
+
+            slow = Reverse(slow);
+            fast = head;
+
+            while (slow != null)
+            {
+                if (fast.val != slow.val)
+                {
+                    return false;
+                }
+
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            return true;
+        }
+
+        private ListNode Reverse(ListNode head)
+        {
+            ListNode prev = null;
+            while (head != null)
+            {
+                ListNode next = head.next;
+                head.next = prev;
+                prev = head;
+                head = next;
+            }
+
+            return prev;
+        }
+    }
+}
